d004838d5a733173033cf9ee1cd71dd735c58f7f
xdvt1337 <76076932+xdvt1337@users.noreply.github.com>
2024-11-20 13:11:47 +0700

    Add files via upload

---
+++ module_14_4/crud_functions.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect("Products.db")
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect("products.db")
+    cursor = connection.cursor()
+
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+
+    connection.close()
+    return products
